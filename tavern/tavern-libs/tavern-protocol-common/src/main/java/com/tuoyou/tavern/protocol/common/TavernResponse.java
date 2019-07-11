f24f888ff24323247af64ab97119395145fa63ce
hebiao <343675896@qq.com>
2019-07-11 23:47:37 +0800

    【tuoyou-alleria一期】规范回参

--- tavern/tavern-libs/tavern-protocol-common/src/main/java/com/tuoyou/tavern/protocol/common/TavernResponse.java
+++ tavern/tavern-libs/tavern-protocol-common/src/main/java/com/tuoyou/tavern/protocol/common/TavernResponse.java
@@ -1,32 +1,27 @@
 package com.tuoyou.tavern.protocol.common;
 
+import lombok.*;
+import lombok.experimental.Accessors;
+
 /**
  * Created by 刘悦之 on 2019/6/30.
  */
+@Builder
+@ToString
+@Accessors(chain = true)
 public class TavernResponse {
-    int retCode;
-    String retMessage;
-
-    public TavernResponse(){}
+    @Getter
+    @Setter
+     int retCode = RetCode.SUCCESS;
+    @Getter
+    @Setter
+     String retMessage = "ok";
 
     public TavernResponse(int retCode, String retMessage) {
         this.retCode = retCode;
         this.retMessage = retMessage;
     }
 
-    public int getRetCode() {
-        return retCode;
-    }
-
-    public void setRetCode(int retCode) {
-        this.retCode = retCode;
-    }
-
-    public String getRetMessage() {
-        return retMessage;
-    }
-
-    public void setRetMessage(String retMessage) {
-        this.retMessage = retMessage;
+    public TavernResponse() {
     }
 }
